a360cfe34aeee7dc8f87755d26e7d76a60902168
mikenegh <mikenegh@gmail.com>
2024-07-18 08:32:46 +0000

    Campaign Initial Form Page Build

--- app/(root)/campaigns/[id]/update/page.tsx
+++ app/(root)/campaigns/[id]/update/page.tsx
@@ -2,19 +2,19 @@ import CampaignForm from '@/components/shared/CampaignForm'
 import { auth } from '@clerk/nextjs/server';
 import React from 'react'
 
-const CreateCampaign = () => {
+const UpdateCampaign = () => {
     const {sessionClaims} = auth();
     const userId = sessionClaims?.userId as string;
   return (
     <>
     <section className = "bg-primary-50 bg-dotted-pattern bg-cover bg-center py-5 md:py-10">
-        <h3 className="wrapper h3-bold text-center sm:text-left">Create Campaign</h3>
+        <h3 className="wrapper h3-bold text-center sm:text-left">Update Campaign</h3>
     </section>
     <div className="wrapper my-8">
-    <CampaignForm userId={userId} type="Create"/>
+    <CampaignForm userId={userId} type="Update"/>
     </div>
     </>
   )
 }
 
-export default CreateCampaign
+export default UpdateCampaign
