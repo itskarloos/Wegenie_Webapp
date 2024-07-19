21a8db5a43225972bc2144c8a0c451e82f4467af
mikenegh <mikenegh@gmail.com>
2024-07-19 07:23:53 +0000

    category button added

--- components/shared/Dropdown.tsx
+++ components/shared/Dropdown.tsx
@@ -1,31 +1,68 @@
-import React from 'react'
+import React, { useState } from "react";
 import {
-    Select,
-    SelectContent,
-    SelectItem,
-    SelectTrigger,
-    SelectValue,
-  } from "@/components/ui/select"
-  
-type DropdownProps={
-    value?: string;
-    onChangeHandler: () => void;
-}
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
 
-const Dropdown = ({value,onChangeHandler}: DropdownProps) => {
+import { ICategory } from "@/lib/database/models/category.model";
+
+type DropdownProps = {
+  value?: string;
+  onChangeHandler: () => void;
+};
+
+const Dropdown = ({ value, onChangeHandler }: DropdownProps) => {
+  const [categories, setCategory] = useState<ICategory[]>([]);
   return (
-    <Select>
-  <SelectTrigger className="w-[180px]">
-    <SelectValue placeholder="Theme" />
-  </SelectTrigger>
-  <SelectContent>
-    <SelectItem value="light">Light</SelectItem>
-    <SelectItem value="dark">Dark</SelectItem>
-    <SelectItem value="system">System</SelectItem>
-  </SelectContent>
-</Select>
+    <Select onValueChange={onChangeHandler} defaultValue={value}>
+      <SelectTrigger className="select-field">
+        <SelectValue placeholder="Category" />
+      </SelectTrigger>
+      <SelectContent>
+        {categories.length > 0 &&
+          categories.map((catagory) => (
+            <SelectItem
+              key={catagory._id}
+              value={catagory._id}
+              className="select-item p-regular-14"
+            >
+              {catagory.name}
+            </SelectItem>
+          ))}
 
-  )
-}
+        <AlertDialog>
+          <AlertDialogTrigger>Open</AlertDialogTrigger>
+          <AlertDialogContent>
+            <AlertDialogHeader>
+              <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+              <AlertDialogDescription>
+                This action cannot be undone. This will permanently delete your
+                account and remove your data from our servers.
+              </AlertDialogDescription>
+            </AlertDialogHeader>
+            <AlertDialogFooter>
+              <AlertDialogCancel>Cancel</AlertDialogCancel>
+              <AlertDialogAction>Continue</AlertDialogAction>
+            </AlertDialogFooter>
+          </AlertDialogContent>
+        </AlertDialog>
+      </SelectContent>
+    </Select>
+  );
+};
 
-export default Dropdown
+export default Dropdown;
--- lib/database/models/category.model.ts
+++ lib/database/models/category.model.ts
@@ -1,5 +1,11 @@
 import { Schema, model, models } from "mongoose";
 
+
+export interface ICategory extends Document {
+    _id: string;
+    name: string;
+  }
+  
 const CategorySchema = new Schema({
     name: {type: String, required: true, unique: true},
 })
