1ed7579a94d513dd7aefa098a6f39e07f5520e04
mikenegh <mikenegh@gmail.com>
2024-07-21 09:58:19 +0000

    Campaign form initial commit

--- components/shared/CampaignForm.tsx
+++ components/shared/CampaignForm.tsx
@@ -22,6 +22,7 @@ import { FileUploader } from "./FileUploader";
 import Image from "next/image";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
+import { Checkbox } from "../ui/checkbox";
 
 type CampaignFormProps = {
   userId: string;
@@ -173,7 +174,6 @@ const CampaignForm = ({ userId, type }: CampaignFormProps) => {
                       timeInputLabel="Time:"
                       dateFormat="MM/dd/yyyy h:mm aa"
                       wrapperClassName="datepicker-wrapper"
-                      
                     />
                   </div>
                 </FormControl>
@@ -182,7 +182,7 @@ const CampaignForm = ({ userId, type }: CampaignFormProps) => {
               </FormItem>
             )}
           />
-    
+
           <FormField
             control={form.control}
             name="endDateTime"
@@ -207,7 +207,6 @@ const CampaignForm = ({ userId, type }: CampaignFormProps) => {
                       timeInputLabel="Time:"
                       dateFormat="MM/dd/yyyy h:mm aa"
                       wrapperClassName="datepicker-wrapper"
-                      
                     />
                   </div>
                 </FormControl>
@@ -218,7 +217,7 @@ const CampaignForm = ({ userId, type }: CampaignFormProps) => {
           />
         </div>
         <div className="flex flex-col gap-5 md:flex-row">
-        <FormField
+          <FormField
             control={form.control}
             name="price"
             render={({ field }) => (
@@ -232,10 +231,64 @@ const CampaignForm = ({ userId, type }: CampaignFormProps) => {
                       height={24}
                       className="filter-grey"
                     />
-                    <Input type="number" placeholder="price" {...field} />
+                    <Input
+                      type="number"
+                      placeholder="Price"
+                      {...field}
+                      className="p-regular-16 border-0 bg-grey-50 outline-offset-0 focus:border-0 focus-visible:ring-0 focus-visible:ring-offset-0"
+                    />
+                    <FormField
+                      control={form.control}
+                      name="isFree"
+                      render={({ field }) => (
+                        <FormItem>
+                          <FormControl>
+                            <div className="flex items-center">
+                              <label
+                                htmlFor="isFree"
+                                className="whitespace-nowrap pr-3 leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+                              >
+                                Free Ticket
+                              </label>
+                              <Checkbox
+                                onCheckedChange={field.onChange}
+                                checked={field.value}
+                                id="isFree"
+                                className="mr-2 h-5 w-5 border-2 border-primary-500"
+                              />
+                            </div>
+                          </FormControl>
+                          <FormMessage />
+                        </FormItem>
+                      )}
+                    />
                   </div>
                 </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="url"
+            render={({ field }) => (
+              <FormItem className="w-full">
+                <FormControl>
+                  <div className="flex-center h-[54px] w-full overflow-hidden rounded-full bg-grey-50 px-4 py-2">
+                    <Image
+                      src="/assets/icons/link.svg"
+                      alt="link"
+                      width={24}
+                      height={24}
+                    />
 
+                    <Input
+                      placeholder="URL"
+                      {...field}
+                      className="input-field"
+                    />
+                  </div>
+                </FormControl>
                 <FormMessage />
               </FormItem>
             )}
