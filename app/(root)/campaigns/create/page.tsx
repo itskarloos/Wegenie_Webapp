15757f0c97f819cff751d6e6f42237e80420a06e
mikenegh <mikenegh@gmail.com>
2024-07-22 15:44:43 +0000

    User Id Fix

--- app/(root)/campaigns/create/page.tsx
+++ app/(root)/campaigns/create/page.tsx
@@ -5,6 +5,7 @@ import { auth } from '@clerk/nextjs/server';
 const CreateCampaign = () => {
     const {sessionClaims} = auth();
     const userId = sessionClaims?.userId as string;
+    console.log(userId)
   return (
     <>
     <section className = "bg-primary-50 bg-dotted-pattern bg-cover bg-center py-5 md:py-10">
