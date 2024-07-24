5829da7877b9925a88905dc85aa9b10c638495c4
mikenegh <mikenegh@gmail.com>
2024-07-24 13:31:41 +0000

    Details Page Setup

--- app/(root)/campaigns/[id]/page.tsx
+++ app/(root)/campaigns/[id]/page.tsx
@@ -1,13 +1,24 @@
-import { getCampaignById } from '@/lib/actions/campaign.actions';
-import { SearchParamProps } from '@/types'
-import React from 'react'
+import { getCampaignById } from "@/lib/actions/campaign.actions";
+import { SearchParamProps } from "@/types";
+import Image from "next/image";
+import React from "react";
 
-const CampaignDetails = async({ params : { id }}: SearchParamProps) => {
+const CampaignDetails = async ({ params: { id } }: SearchParamProps) => {
   const campaign = await getCampaignById(id);
-  console.log(campaign)
+  console.log(campaign);
   return (
-    <div>page</div>
-  )
-}
+    <section className="flex justify-center bg-primary-50 bg-dotted-pattern bg-contain">
+      <div className="grid gird-col-1 md:grid-cols-2 2xl:max-w-7xl">
+        <Image
+          src={campaign.imageUrl}
+          alt="hero image"
+          width={1000}
+          height={1000}
+          className="h-full min-h-[300px] object-cover object-center"
+        />
+      </div>
+    </section>
+  );
+};
 
-export default CampaignDetails
+export default CampaignDetails;
