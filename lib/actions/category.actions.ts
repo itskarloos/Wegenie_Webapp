f461c57ee2d6ad0b8fe37aa63af3d142ac62c4d1
mikenegh <mikenegh@gmail.com>
2024-07-24 08:50:35 +0000

    Some Change

--- lib/actions/category.actions.ts
+++ lib/actions/category.actions.ts
@@ -1,33 +1,30 @@
 "use server"
 
 import { CreateCategoryParams } from "@/types"
+import { handleError } from "../utils"
 import { connectToDatabase } from "../database"
-import Category from "../database/models/category.model";
-import { handleError } from "../utils";
+import Category from "../database/models/category.model"
 
+export const createCategory = async ({ categoryName }: CreateCategoryParams) => {
+  try {
+    await connectToDatabase();
 
-export const createCategory = async({categoryName}: CreateCategoryParams) => {
-    try{
-        await connectToDatabase();
+    const newCategory = await Category.create({ name: categoryName });
 
-        const newCategory = await Category.create({name : categoryName});
-        return JSON.parse(JSON.stringify(newCategory));
-    }
-    catch(error){
-
-        handleError(error);
-    }
+    return JSON.parse(JSON.stringify(newCategory));
+  } catch (error) {
+    handleError(error)
+  }
 }
 
-export const getAllCategories = async() => {
-    try{
-        await connectToDatabase();
+export const getAllCategories = async () => {
+  try {
+    await connectToDatabase();
 
-        const Categories = await Category.find();
-        return JSON.parse(JSON.stringify(Categories));
-    }
-    catch(error){
+    const categories = await Category.find();
 
-        handleError(error);
-    }
+    return JSON.parse(JSON.stringify(categories));
+  } catch (error) {
+    handleError(error)
+  }
 }
