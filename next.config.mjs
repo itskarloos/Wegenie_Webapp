c573e04565b1a90c54df207a10db6163c9300c48
mikenegh <mikenegh@gmail.com>
2024-07-24 14:15:13 +0000

    Next Config Change

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images:{
-        domains:['utfs.io']
+        domains:['utfs.io'],
+        remotePatterns:[
+            {
+                protocol:'https',
+                hostname:'utfs.io',
+                port:'',
+                
+            }
+        ]
     }
 };
 
