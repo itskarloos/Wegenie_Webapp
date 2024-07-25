2b50fbbbb1e83fc81194ab6fb07ab0d83297b4dd
mikenegh <mikenegh@gmail.com>
2024-07-25 12:34:14 +0000

    Campaign Fix

--- components/shared/Card.tsx
+++ components/shared/Card.tsx
@@ -9,7 +9,7 @@ type CardProps = {
 const Card = ( {campaign,hasOrderLink,hidePrice} : CardProps) => {
   return (
     <div className='group relative flex min-h-[380px] w-full max-w-[400px] flex-col overflow-hidden rounded-xl bg-white shadow-md transition-all hover:shadow-lg md:min-h-[438px]'>
-      <Link href={`/campaings/${campaign._id}`} style={{backgroundImage: `url(${campaign.imageUrl})`}}
+      <Link href={`/campaigns/${campaign._id}`} style={{backgroundImage: `url(${campaign.imageUrl})`}}
       className='flex-center flex-grow bg-grey-50 bg-cover bg-center text-grey-500'
       >
       </Link>
