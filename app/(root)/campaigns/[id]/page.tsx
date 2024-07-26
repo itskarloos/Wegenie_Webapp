0605042854708645e0ba77c4d68a8c7c6b7d57be
mikenegh <mikenegh@gmail.com>
2024-07-26 08:59:56 +0000

    Card Description

--- app/(root)/campaigns/[id]/page.tsx
+++ app/(root)/campaigns/[id]/page.tsx
@@ -23,7 +23,7 @@ const CampaignDetails = async ({ params: { id } }: SearchParamProps) => {
             <div className="flex flex-col gap-3 sm:flex-row sm:items-center">
               <div className="flex gap-3">
                 <p className="p-bold-20 rounded-full bg-green-500/10 px-5 py-2 text-green-700">
-                  {campaign.isFree ? "FREE" : `${campaign.price}`}
+                  {campaign.isFree ? "FREE" : `$${campaign.price}`}
                 </p>
                 <p className="p-medium-16 rounded-full bg-grey-500/10 px-4 py-2.5 text-grey-500">
                   {campaign.category.name}
--- components/shared/Card.tsx
+++ components/shared/Card.tsx
@@ -1,22 +1,52 @@
-import { ICampaign } from '@/lib/database/models/campaign.model'
-import Link from 'next/link'
-import React from 'react'
+import { ICampaign } from "@/lib/database/models/campaign.model";
+import { formatDateTime } from "@/lib/utils";
+import Link from "next/link";
+import React from "react";
 type CardProps = {
-  campaign: ICampaign,
-  hasOrderLink?: boolean,
-  hidePrice?: boolean
-}
-const Card = ( {campaign,hasOrderLink,hidePrice} : CardProps) => {
-console.log(campaign)
+  campaign: ICampaign;
+  hasOrderLink?: boolean;
+  hidePrice?: boolean;
+};
+const Card = ({ campaign, hasOrderLink, hidePrice }: CardProps) => {
+  console.log(campaign);
   return (
-    <div className='group relative flex min-h-[380px] w-full max-w-[400px] flex-col overflow-hidden rounded-xl bg-white shadow-md transition-all hover:shadow-lg md:min-h-[438px]'>
-      <Link 
+    <div className="group relative flex min-h-[380px] w-full max-w-[400px] flex-col overflow-hidden rounded-xl bg-white shadow-md transition-all hover:shadow-lg md:min-h-[438px]">
+      <Link
         href={`/campaigns/${campaign._id}`}
-        style={{backgroundImage: `url(${campaign.imageUrl})`}}
+        style={{ backgroundImage: `url(${campaign.imageUrl})` }}
         className="flex-center flex-grow bg-gray-50 bg-cover bg-center text-grey-500"
       />
-      </div>
-  )
-}
 
-export default Card
+      <Link
+        href={`/campaigns/${campaign._id}`}
+        className="flex min-h-[230px] flex-col gap-3 p-5 md:gap-4"
+      >
+        {!hidePrice && (
+          <div className="flex gap-2">
+            <span className="p-semibold-14 w-min rounded-full bg-green-100 px-4 py-1 text-green-60">
+              {campaign.isFree ? "FREE" : `$${campaign.price}`}
+            </span>
+            <p className="p-semibold-14 w-min rounded-full bg-grey-500/10 px-4 py-1 text-grey-500">
+              {campaign.category.name}
+            </p>
+          </div>
+          
+        )}
+        <p className="p-medium-16 md:p-medium-18 text-grey-500">
+            {formatDateTime(campaign.startDateTime).dateTime}
+          </p>
+          <p className="p-medium-16 md:p-medium-20 line-clamp-2 flex-1 text-black">{campaign.title}</p>
+          <div className="flex-between w-full">
+            <p className="p-medium-14 md:p-medium-16 text-grey-600">
+              {campaign.organizer.firstName} {campaign.organizer.lastName}
+            </p>
+            {hasOrderLink &&(<Link href={'/orders?campaignId=${campaign._id}'} className="flex gap-2">
+              <p className="text-primary-500">Order Details</p>
+            </Link>)}
+          </div>
+      </Link>
+    </div>
+  );
+};
+
+export default Card;
