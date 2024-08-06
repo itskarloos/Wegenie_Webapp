4dd5481f049e991db9aa62f9e16035cc84e265ed
mikenegh <mikenegh@gmail.com>
2024-08-06 09:59:38 +0000

    Orders page

--- components/shared/Collection.tsx
+++ components/shared/Collection.tsx
@@ -29,7 +29,7 @@ const Collection = ({
         <div className="flex flex-col items-center gap-10">
           <ul className="grid w-full grid-cols-1 gap-5 sm:grid-cols-2 lg:grid-cols-3 xl:gap-10">
             {data.map((campaign) => {
-              const hasOrderLink = collectionType === 'Campaigns_Organized';
+              const hasOrderLink = collectionType === 'Campaigns_Organized' ;
               const hidePrice = collectionType === 'Campaigns_Created';
               
             
