7bafb865834786d1dfee98e7624e2db8ecfbd451
mikenegh <mikenegh@gmail.com>
2024-08-08 14:33:16 +0000

    Category Fetch

--- components/shared/CategoryFilter.tsx
+++ components/shared/CategoryFilter.tsx
@@ -1,4 +1,5 @@
-import React from 'react'
+"use client"
+import React, { useEffect, useState } from 'react'
 import {
     Select,
     SelectContent,
@@ -6,17 +7,65 @@ import {
     SelectTrigger,
     SelectValue,
   } from "@/components/ui/select"
+import { useRouter, useSearchParams } from 'next/navigation';
+import { formUrlQuery, removeKeysFromQuery } from '@/lib/utils';
+import { ICategory } from '@/lib/database/models/category.model';
+import { getAllCategories } from '@/lib/actions/category.actions';
   
 const CategoryFilter = () => {
+
+    const [categories,setCategories] = useState<ICategory[]>([]);
+    const searchParams = useSearchParams();
+    const router = useRouter();
+    
+    useEffect(() => {
+        const getCategories = async () => {
+          const categoryList = await getAllCategories();
+    
+          categoryList && setCategories(categoryList as ICategory[])
+        }
+    
+        getCategories();
+      }, [])
+
+
+      
+    useEffect(()=>{
+        const delayDebounceFn = setTimeout(()=>{
+            let newUrl= '';
+            if(categories){
+                 newUrl = formUrlQuery({
+                    params: searchParams.toString(),
+                    key: 'categories',
+                    value: categories
+                })
+            }
+            else {
+                newUrl= removeKeysFromQuery({
+                    params: searchParams.toString(),
+                    keysToRemove: ['categories']
+                })
+            }
+            router.push(newUrl,{scroll: false})
+        },300)
+        return ()=> clearTimeout(delayDebounceFn);
+
+    },[categories,searchParams,router])
+
+    const onSubmitCategory = (category: string)=>{}
+    
   return (
-    <Select>
-    <SelectTrigger className="w-[180px]">
-      <SelectValue placeholder="Theme" />
+    <Select onValueChange={(value:string)=>onSubmitCategory(value)}>
+    <SelectTrigger className="select-field">
+      <SelectValue placeholder="Category" />
     </SelectTrigger>
     <SelectContent>
-      <SelectItem value="light">Light</SelectItem>
-      <SelectItem value="dark">Dark</SelectItem>
-      <SelectItem value="system">System</SelectItem>
+      <SelectItem value="All" className='select-item p-regular-14'>All</SelectItem>
+     {categories.map((category)=>{
+        return (
+            <SelectItem key={category._id} value={category.name} className='select-item p-regular-14'>{category.name}</SelectItem>
+        )
+     })}
     </SelectContent>
   </Select>
   
