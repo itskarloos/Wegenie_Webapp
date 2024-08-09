8ca9b51061b604bd7ee7fd772fddb9bb5f23eaf2
mikenegh <mikenegh@gmail.com>
2024-08-09 06:44:23 +0000

    Pagination Functionality

--- components/shared/Pagination.tsx
+++ components/shared/Pagination.tsx
@@ -2,32 +2,48 @@
 import { useRouter, useSearchParams } from 'next/navigation';
 import React from 'react'
 import { Button } from '../ui/button';
+import { formUrlQuery } from '@/lib/utils';
 
 
-type PaginationProps ={
+type PaginationProps = {
     page: number | string,
     totalPages: number,
     urlParamName?: string
-    
+
 }
-const Pagination = ({page,totalPages,urlParamName}:PaginationProps) => {
+const Pagination = ({ page, totalPages, urlParamName }: PaginationProps) => {
     const router = useRouter();
     const searchParams = useSearchParams();
 
 
     const onClick = (btnType: string) => {
+        const pageValue = btnType === "next" ?
+            (Number(page) + 1) :
+            (Number(page) - 1)
 
+        const newUrl = formUrlQuery(
+            {
+                params: searchParams.toString(),
+                key: urlParamName || "page",
+                value: pageValue.toString()
+            }
+        )
+
+
+
+        router.push(newUrl,{scroll:false})
     }
-  return (
-    <div className="flex gap-2">
-        <Button className = "w-28" size="lg" variant="outline" onClick={() => onClick("prev")} disabled={Number(page) <=1}>
-            Previous
-        </Button>
-        <Button className = "w-28" size="lg" variant="outline" onClick={() => onClick("next")} disabled={Number(page) >=totalPages}>
-            Next
-        </Button>
-    </div>
-  )
+
+    return (
+        <div className="flex gap-2">
+            <Button className="w-28" size="lg" variant="outline" onClick={() => onClick("prev")} disabled={Number(page) <= 1}>
+                Previous
+            </Button>
+            <Button className="w-28" size="lg" variant="outline" onClick={() => onClick("next")} disabled={Number(page) >= totalPages}>
+                Next
+            </Button>
+        </div>
+    )
 }
 
 export default Pagination
