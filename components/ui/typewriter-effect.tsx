5b515604c2881eb1ae3743e37ffda71410e3804c
mikenegh <mikenegh@gmail.com>
2024-08-12 10:52:56 +0000

    Type write fix

--- components/ui/typewriter-effect.tsx
+++ components/ui/typewriter-effect.tsx
@@ -157,9 +157,7 @@ export const TypewriterEffectSmooth = ({
       >
         <div
           className="text-xs sm:text-base md:text-xl lg:text:3xl xl:text-5xl font-bold"
-          style={{
-            whiteSpace: "wrap",
-          }}
+          
         >
           {renderWords()}{" "}
         </div>{" "}
