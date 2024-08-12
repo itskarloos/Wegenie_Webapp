07d63ac2bd50a51bb33532f3eded6a6e4b684a14
mikenegh <mikenegh@gmail.com>
2024-08-12 08:29:23 +0000

    Padding Left Change

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -17,7 +17,7 @@ export default async function Home({searchParams}: SearchParamProps) {
   return (
     <>
       <section className="bg-primary-50 bg-dotted-pattern bg-contain py-8 md:py-10">
-        <div className="wrapper grid grid-col-1 gap-5 md:grid-cols-2 2xl:gap-0">
+        <div className="wrapper grid grid-col-1 gap-2 md:grid-cols-2 2xl:gap-0">
           <div className="flex flex-col justify-center gap-5">
             <h1 className="h1-bold">
               Start Cultivating Hope, Change Life's, Impact The World Now!
