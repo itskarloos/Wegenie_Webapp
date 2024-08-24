2a8fa37021a91e6aadfa4ef970fea7057db6ddae
mikenegh <mikenegh@gmail.com>
2024-08-24 14:34:53 +0000

    Verfication Badge Added ✅

--- components/shared/Card.tsx
+++ components/shared/Card.tsx
@@ -6,11 +6,9 @@ import Image from "next/image";
 import Link from "next/link";
 import React, { useState } from "react";
 import { DeleteConfirmation } from "./DeleteConfirmation";
-import { Progress } from "@/components/ui/progress"
-import { CircleCheck, BadgeCheck } from 'lucide-react';
-
-
-
+import { Progress } from "@/components/ui/progress";
+import { CircleCheck, BadgeCheck } from "lucide-react";
+import { Badge } from "@/components/ui/badge";
 
 type CardProps = {
   campaign: ICampaign;
@@ -21,10 +19,9 @@ const Card = ({ campaign, hasOrderLink, hidePrice }: CardProps) => {
   const { sessionClaims } = auth();
   const userId = sessionClaims?.userId as String;
   const isCampaignCreator = userId === campaign.organizer._id.toString();
-  const progress = (parseFloat(campaign.donatedAmount) / parseFloat(campaign.campaignAmount)) * 100;
-
-
-
+  const progress =
+    (parseFloat(campaign.donatedAmount) / parseFloat(campaign.campaignAmount)) *
+    100;
 
   return (
     <div className="group relative flex min-h-[380px] w-full max-w-[400px] flex-col overflow-hidden rounded-xl bg-white shadow-md transition-all hover:shadow-lg md:min-h-[438px]">
@@ -35,24 +32,34 @@ const Card = ({ campaign, hasOrderLink, hidePrice }: CardProps) => {
       />
       {isCampaignCreator && (
         <div className="absolute right-2 top-2 flex flex-col gap-4 rounded-xl bg-white p-3 shadow-sm transition-all">
-          <Link href={`/campaigns/${campaign._id}/update`} >
-            <Image src="/assets/icons/edit.svg" alt="edit" width={20} height={20} />
+          <Link href={`/campaigns/${campaign._id}/update`}>
+            <Image
+              src="/assets/icons/edit.svg"
+              alt="edit"
+              width={20}
+              height={20}
+            />
           </Link>
           <DeleteConfirmation campaignId={campaign._id} />
         </div>
-
       )}
-      <div
-        className="flex min-h-[230px] flex-col gap-2 p-5 md:gap-3"
-      >
+      <div className="flex min-h-[230px] flex-col gap-2 p-5 md:gap-3">
         <Link href={`/campaigns/${campaign._id}`}>
-          <div
-            className="flex flex-row items-center"
-          >
-            <p className="p-medium-16 md:p-medium-20 line-clamp-2 flex-1 text-black py-2">{campaign.title} </p> {campaign.isVerified && (<BadgeCheck color="green" strokeWidth={3}/>)}
+          <div className="flex flex-row items-center">
+            <p className="p-medium-16 md:p-medium-20 line-clamp-2 flex-1 text-black py-2">
+              {campaign.title}{" "}
+            </p>{" "}
+            {campaign.isVerified && (
+              <Badge
+                variant="outline"
+                className="bg-primary text-primary-foreground px-3 py-1 rounded-full flex items-center justify-center gap-2"
+              >
+                <CircleCheck color="white" className="h-4 w-4" />
+                <span className="text-sm font-medium">Verified</span>
+              </Badge>
+            )}
           </div>
           <Progress value={progress} className="w-[100%]" />
-
         </Link>
 
         {!hidePrice && (
@@ -69,24 +76,30 @@ const Card = ({ campaign, hasOrderLink, hidePrice }: CardProps) => {
               {campaign.donatedAmount}$ • Raised
             </p>
           </div>
-
         )}
-
 
         <p className="p-regular-12 md:p-regular-14 text-grey-500">
           {formatDateTime(campaign.startDateTime).dateTime}
         </p>
 
-
-
         <div className="flex-between w-full">
           <p className="p-medium-14 md:p-medium-16 text-grey-600">
             by {campaign.organizer.firstName} {campaign.organizer.lastName}
           </p>
-          {hasOrderLink && (<Link href={`/orders?campaignId=${campaign._id}`} className="flex gap-2">
-            <p className="text-primary-500">Order Details</p>
-            <Image src="/assets/icons/arrow.svg" height={10} width={10} alt="Search" />
-          </Link>)}
+          {hasOrderLink && (
+            <Link
+              href={`/orders?campaignId=${campaign._id}`}
+              className="flex gap-2"
+            >
+              <p className="text-primary-500">Order Details</p>
+              <Image
+                src="/assets/icons/arrow.svg"
+                height={10}
+                width={10}
+                alt="Search"
+              />
+            </Link>
+          )}
         </div>
       </div>
     </div>
