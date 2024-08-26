05e400042b8619ee122af5a25c9e56fac839f2f5
mikenegh <mikenegh@gmail.com>
2024-08-26 16:18:43 +0000

    debug

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -9,7 +9,7 @@ export default function RootLayout({
     return (
       <div className = "flex h-screen flex-col">
         <Header />
-        <main className = "flex-1 mt-[5]">{children}</main>
+        <main className = "flex-1 mt-10">{children}</main>
         <Footer />
       </div>
     );
